cfb1580f1ed1daefb015f94c2134c3f5eeb29002
Aleksandr <chy4egg@gmail.com>
2018-08-24 21:46:37 +0300

    proto inheritance 2 was added

---
+++ prototypes_inheritance2.js
@@ -0,0 +1,31 @@
+console.clear();
+
+var Duck = function(name) {
+  this.name = name;
+};
+
+Duck.prototype.quack = function() {
+    return this.name +" Duck: Quack-quack!";
+};
+
+/// Наследование
+var TalkingDuck = function(name) {
+  Duck.call(this, name);
+}
+
+TalkingDuck.prototype = Object.create(Duck.prototype);
+TalkingDuck.prototype.constructor = TalkingDuck;
+
+TalkingDuck.prototype.quack = function() {
+    return Duck.prototype.quack.call(this) + " My name is " + this.name;
+};
+
+/// Инстанцирование
+
+// обычная утка (наследование от Duck)
+var regularDuck = new Duck('Just');
+console.log(regularDuck.quack());
+
+// утка Donald (наследование от TalkingDuck)
+var donald = new TalkingDuck("Donald");
+console.log( donald.quack() );
