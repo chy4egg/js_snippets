fe40b095ec74f62f22b78c3637845180248972cb
Aleksandr <chy4egg@gmail.com>
2019-01-20 13:01:19 +0300

    save changes

---
+++ currying/example.js
@@ -0,0 +1,19 @@
+//Currying is translating a function from callable as f(a,b) into f(a)(b)
+
+//example
+function curry(func) {
+    return function(a) {
+        return function(b) {
+            return func(a,b);
+        }
+    }
+}
+
+//usage
+function sum(a,b) {
+    return a + b;
+}
+
+var curriedSum = curry(sum);
+
+console.log(curriedSum(1)(2)); // 3
---
+++ playground/test.js
@@ -0,0 +1,5 @@
+function test(...rest) {
+  console.log(rest);
+}
+
+test(1,2,3,4,5);
